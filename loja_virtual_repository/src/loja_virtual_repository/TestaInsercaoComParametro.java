186a46a80a0eefb9b20111eac61c6451efe13ff8
Larissa Bandeira de Jesus <larissaband13@gmail.com>
2023-01-27 15:11:08 -0300

    commit e rollback

--- loja_virtual_repository/src/loja_virtual_repository/TestaInsercaoComParametro.java
+++ loja_virtual_repository/src/loja_virtual_repository/TestaInsercaoComParametro.java
@@ -15,25 +15,31 @@ public class TestaInsercaoComParametro {
 		ConnectionFactory connectionFactory = new ConnectionFactory();
 		Connection conexao = connectionFactory.recuperarConexao();
 		conexao.setAutoCommit(false); // controlando 
-		 
-		PreparedStatement stm = conexao.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS);
 		
-		adicionarVariavel("SmartTV", "45 polegadas", stm);
-		adicionarVariavel("Radio", "Radio bluetooth", stm);
-		
-		conexao.commit();
-		
-		stm.close();
-		conexao.close();
+		try {
+			PreparedStatement stm = conexao.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS);
+			
+			adicionarVariavel("SmartTV", "45 polegadas", stm);
+			adicionarVariavel("Radio", "Radio bluetooth", stm);
+			
+			conexao.commit();
+			
+			stm.close();
+			conexao.close();
+		}catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("ROLLBACK EXECUTADO");
+			conexao.rollback();
+		}
 	}
 
 	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
 		stm.setString(1, nome);
 		stm.setString(2, descricao);
 		
-		if(nome.equals("teste")) {
-			throw new RuntimeException("Nao foi possivel adicionar o produto");
-		}
+//		if(nome.equals("Radio")) {
+//			throw new RuntimeException("Nao foi possivel adicionar o produto");
+//		}
 		
 		stm.execute();
 		ResultSet rst = stm.getGeneratedKeys();
