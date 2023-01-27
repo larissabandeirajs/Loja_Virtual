480731db99401a7a928bb02c6d02563d057d226e
Larissa Bandeira de Jesus <larissaband13@gmail.com>
2023-01-27 15:05:11 -0300

    commit e rollback

--- loja_virtual_repository/src/loja_virtual_repository/TestaInsercaoComParametro.java
+++ loja_virtual_repository/src/loja_virtual_repository/TestaInsercaoComParametro.java
@@ -14,18 +14,24 @@ public class TestaInsercaoComParametro {
 		
 		ConnectionFactory connectionFactory = new ConnectionFactory();
 		Connection conexao = connectionFactory.recuperarConexao();
+		conexao.setAutoCommit(false); // controlando 
 		 
 		PreparedStatement stm = conexao.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS);
 		
 		adicionarVariavel("SmartTV", "45 polegadas", stm);
 		adicionarVariavel("Radio", "Radio bluetooth", stm);
+		
+		conexao.commit();
+		
+		stm.close();
+		conexao.close();
 	}
 
 	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
 		stm.setString(1, nome);
 		stm.setString(2, descricao);
 		
-		if(nome.equals("Radio")) {
+		if(nome.equals("teste")) {
 			throw new RuntimeException("Nao foi possivel adicionar o produto");
 		}
 		
