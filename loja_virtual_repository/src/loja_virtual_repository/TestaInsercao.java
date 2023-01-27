d602af768c200c72af67a3d730bcd8bfe2309f5e
Larissa Bandeira de Jesus <larissaband13@gmail.com>
2023-01-27 13:34:22 -0300

    Teste ID

--- loja_virtual_repository/src/loja_virtual_repository/TestaInsercao.java
+++ loja_virtual_repository/src/loja_virtual_repository/TestaInsercao.java
@@ -1,6 +1,7 @@
 package loja_virtual_repository;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -12,9 +13,16 @@ public class TestaInsercao {
 		Connection conexao = connectionFactory.recuperarConexao();
 		
 		Statement stm = conexao.createStatement();
-		boolean resultado = 
-		stm.execute("INSERT INTO PRODUTO (nome, descricao) VALUES ('Mouse', 'Mouse sem fio' )");
-		System.out.println(resultado);
+		 
+		stm.execute("INSERT INTO PRODUTO (nome, descricao) VALUES ('Mouse', 'Mouse sem fio' )"
+				, Statement.RETURN_GENERATED_KEYS);
+		ResultSet rst = stm.getGeneratedKeys();
+		
+		 while(rst.next()) {
+			 Integer id = rst.getInt(1);
+			 System.out.println("O ID criado foi: " + id);
+		 }
+		 
 
 	}
 
