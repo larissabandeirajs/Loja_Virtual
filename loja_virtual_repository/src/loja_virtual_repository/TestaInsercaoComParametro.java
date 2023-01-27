65ab27afadff0db2aefcce50df564202e81ab6ad
Larissa Bandeira de Jesus <larissaband13@gmail.com>
2023-01-27 15:24:55 -0300

    try-with-resources

--- loja_virtual_repository/src/loja_virtual_repository/TestaInsercaoComParametro.java
+++ loja_virtual_repository/src/loja_virtual_repository/TestaInsercaoComParametro.java
@@ -6,49 +6,46 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
 public class TestaInsercaoComParametro {
-	
+
 	public static void main(String[] args) throws SQLException {
-		 
-		
+
 		ConnectionFactory connectionFactory = new ConnectionFactory();
-		Connection conexao = connectionFactory.recuperarConexao();
-		conexao.setAutoCommit(false); // controlando 
-		
-		try {
-			PreparedStatement stm = conexao.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS);
-			
-			adicionarVariavel("SmartTV", "45 polegadas", stm);
-			adicionarVariavel("Radio", "Radio bluetooth", stm);
-			
-			conexao.commit();
-			
-			stm.close();
-			conexao.close();
-		}catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("ROLLBACK EXECUTADO");
-			conexao.rollback();
+		try (Connection conexao = connectionFactory.recuperarConexao()) {
+
+			conexao.setAutoCommit(false); // controlando
+
+			try (PreparedStatement stm = conexao.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)",
+					Statement.RETURN_GENERATED_KEYS);) {
+				adicionarVariavel("SmartTV", "45 polegadas", stm);
+				adicionarVariavel("Radio", "Radio bluetooth", stm);
+
+				conexao.commit();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("ROLLBACK EXECUTADO");
+				conexao.rollback();
+			}
 		}
 	}
 
 	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
 		stm.setString(1, nome);
 		stm.setString(2, descricao);
-		
+
 //		if(nome.equals("Radio")) {
 //			throw new RuntimeException("Nao foi possivel adicionar o produto");
 //		}
-		
+
 		stm.execute();
-		ResultSet rst = stm.getGeneratedKeys();
-		
-		 while(rst.next()) {
-			 Integer id = rst.getInt(1);
-			 System.out.println("O ID criado foi: " + id);
-		 }
-		 rst.close();
+
+		try (ResultSet rst = stm.getGeneratedKeys()) {
+			while (rst.next()) {
+				Integer id = rst.getInt(1);
+				System.out.println("O ID criado foi: " + id);
+			}
+		}
 	}
 
 }
