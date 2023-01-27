f948e99a3a1aa2351223acfa6f337c54f0fdc444
Larissa Bandeira de Jesus <larissaband13@gmail.com>
2023-01-27 11:30:59 -0300

    Conexão JDBC

---
+++ loja_virtual_repository/src/loja_virtual_repository/TestaConexao.java
@@ -0,0 +1,20 @@
+package loja_virtual_repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestaConexao {
+	
+	public static void main(String[] args) throws SQLException {
+		
+   Connection connection = DriverManager.getConnection(
+		"jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC",
+		"root","123456");
+	
+   System.out.println("Fechando conexao!");
+   
+   connection.close();   
+}
+
+}
---
+++ loja_virtual_repository/src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author Visua
+ *
+ */
+module loja_virtual_repository {
+	requires java.sql;
+}
